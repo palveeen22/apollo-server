741c0ffe775f8958a2a65d39d252a6892b9f36c6
Alvin Pratama <palveeen22@gmail.com>
2024-05-31 09:00:21 +0700

    fix: fixing config setup

--- config/redis.js
+++ config/redis.js
@@ -1,17 +1,17 @@
 const Redis = require("ioredis");
 
 const redis = new Redis({
-  port: 16663, // Redis port
-  host: "redis-16663.c292.ap-southeast-1-1.ec2.cloud.redislabs.com", // Redis host
-  password: "UFvJV2aeZb8zvaiKpp9tpOsYuKRlBFlA",
+	port: process.env.REDIS_PORT,
+	host: process.env.REDIS_HOST || "redis_host",
+	password: process.env.REDIS_PASSWORD || "password",
 });
 
 redis.on("conect", () => {
-  console.log("Succes connect redis");
+	console.log("Succes connect redis");
 });
 
 redis.on("error", (error) => {
-  console.error("Error connecting to Redis", error);
+	console.error("Error connecting to Redis", error);
 });
 
 module.exports = redis;
